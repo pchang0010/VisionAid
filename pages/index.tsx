ff5d41f02b3211791ab9df6ea08c3f844770ee33
Phillip Chang <pchang78@gatech.edu>
2022-02-21 11:03:36 -0800

    Move navbar to static page render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Navbar } from "../components/Navbar";
 import type { NextPage } from "next";
-import { Container, Text, Stack, Image } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Image,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Spacer,
+  Button,
+  Container,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
 
 const Root = styled.div`
   height: 100vh;
@@ -12,7 +24,59 @@ const Home: NextPage = () => {
   return (
     <body>
       <Root>
-        <Navbar />
+        <Flex as="nav" align="center">
+          <Box>
+            <Image
+              src="banner.webp"
+              htmlHeight={200}
+              htmlWidth={300}
+              onClick={() => {
+                window.location.href = "/";
+              }}
+              style={{ cursor: "pointer" }}
+            />
+          </Box>
+          <Box>
+            <Button
+              variant="ghost"
+              onClick={() => {
+                window.location.href = "/";
+              }}
+            >
+              Home
+            </Button>
+            <Menu autoSelect={false}>
+              <MenuButton as={Button} variant="ghost">
+                Students
+              </MenuButton>
+              <MenuList>
+                <MenuItem>List students</MenuItem>
+                <MenuItem>Add a student</MenuItem>
+              </MenuList>
+            </Menu>
+            <Menu autoSelect={false}>
+              <MenuButton as={Button} variant="ghost">
+                Courses
+              </MenuButton>
+              <MenuList>
+                <MenuItem>List courses</MenuItem>
+                <MenuItem>Add a course</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+          <Spacer />
+          <Menu autoSelect={false}>
+            <MenuButton as={Button} variant="ghost">
+              My Account
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Account settings</MenuItem>
+              <MenuItem>Log out</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+
+        {/* NON NAVBAR CODE STARTS HERE */}
         <Container>
           <Stack>
             <Text fontSize="xx-large">About us</Text>
