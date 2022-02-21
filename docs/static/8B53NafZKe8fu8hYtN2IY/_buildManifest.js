640bbfe08e30eb94a733d587fac87c30fa4dd30d
Phillip Chang <pchang78@gatech.edu>
2022-02-21 10:44:40 -0800

    Fixed static html export

--- docs/static/8B53NafZKe8fu8hYtN2IY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/846-0f72f9dd3bd25a5e.js","static/chunks/pages/index-04305aa5ae8e3821.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
